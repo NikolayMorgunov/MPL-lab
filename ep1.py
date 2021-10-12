e32597862f3220d0bd9d6c5286e1d09eff7a2550
Darkkvod <da>
2021-10-12 18:02:17 +0300

    Episode 1 ready

--- ep1.py
+++ ep1.py
@@ -1,1 +1,17 @@
-import matplotlib
+import matplotlib.pyplot as plt
+
+file_numb = input('File number = ')
+with open(f'data/00{file_numb}.dat', 'r') as f:
+    s = f.read().split('\n')
+    n = int(s[0])
+    x, y = [], []
+    for i in s[1:n + 1]:
+        x.append(float(i.split()[0]))
+        y.append(float(i.split()[1]))
+    size = [int(input('Points size = '))] * n
+    plt.scatter(x, y, size)
+    plt.axis('scaled')
+    plt.title('Number of points: ' + str(n))
+    plt.show()
+    plt.savefig('plot.png')
+
